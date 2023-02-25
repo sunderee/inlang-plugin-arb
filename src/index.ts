2972d0263b56561a619360dedd17c24edf04c216
Peter Aleksander Bizjak <peteralexbizjak@protonmail.com>
2023-02-25 15:32:40 +0100

    Last cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,37 @@
 import type * as ast from "@inlang/core/ast";
-import type { Config, EnvironmentFunctions } from "@inlang/core/config";
+import type { Config } from "@inlang/core/config";
 
-export type PluginConfig = {
-  pathPattern: string;
-};
-
-export async function readResources(
-  args: Parameters<Config["readResources"]>[0] &
-    EnvironmentFunctions & { pluginConfig: PluginConfig }
-): ReturnType<Config["readResources"]> {
+/**
+ * Reading resources. At the moment, the plugin does not support custom path
+ * pattern for reading resources, as it assumes you're following the official
+ * documentation guidelines:
+ *
+ * https://docs.flutter.dev/development/accessibility-and-localization/internationalization
+ *
+ * As a recap, you should have a `l10n.yaml` with contents similar to:
+ *
+ * ```yaml
+ * arb-dir: lib/l10n
+ * template-arb-file: app_en.arb
+ * output-localization-file: app_localizations.dart
+ * ```
+ *
+ * Therefore, the path pattern follows `./lib/l10n/app_{language}.arb`
+ *
+ * @param args inlang config schema
+ * @returns list of resources
+ */
+export async function readResources(args: {
+  config: Config;
+}): Promise<ast.Resource[]> {
   const result: ast.Resource[] = [];
   for (const language of args.config.languages) {
-    const resourcePath = args.pluginConfig.pathPattern.replace(
-      "{language}",
-      language
-    );
-    const arbContent = (await args.$fs.readFile(
-      resourcePath,
-      "utf-8"
-    )) as string;
-    const resource = parseResource(arbContent, language);
-    result.push(resource);
   }
+
   return result;
 }
 
-export async function writeResources(
-  args: Parameters<Config["writeResources"]>[0] &
-    EnvironmentFunctions & { pluginConfig: PluginConfig }
-): ReturnType<Config["writeResources"]> {
-  for (const resource of args.resources) {
-    const resourcePath = args.pluginConfig.pathPattern.replace(
-      "{language}",
-      resource.languageTag.name
-    );
-    const arbContent = serializeResource(resource);
-    await args.$fs.writeFile(resourcePath, arbContent);
-  }
-}
-
-function parseResource(arbContent: string, language: string): ast.Resource {
-  const arbObject = JSON.parse(arbContent);
-  return {
-    type: "Resource",
-    languageTag: {
-      type: "LanguageTag",
-      name: language,
-    },
-    body: Object.entries(arbObject).map(([id, message]) =>
-      parseMessage(id, message)
-    ),
-  };
-}
-
-function parseMessage(id: string, message: any): ast.Message {
-  return {
-    type: "Message",
-    id: {
-      type: "Identifier",
-      name: id,
-    },
-    pattern: {
-      type: "Pattern",
-      elements: [{ type: "Text", value: message.toString() }],
-    },
-  };
-}
-
-function serializeResource(resource: ast.Resource): string {
-  const arbObject = Object.fromEntries(resource.body.map(serializeMessage));
-  return JSON.stringify(arbObject, null, 2);
-}
-
-function serializeMessage(message: ast.Message): [id: string, value: any] {
-  return [message.id.name, message.pattern.elements[0].value];
-}
+export async function writeResources(args: {
+  config: Config;
+  resources: ast.Resource[];
+}): Promise<void> {}
